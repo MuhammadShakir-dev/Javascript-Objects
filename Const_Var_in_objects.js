a47b153fe9b8f51930ca8331cbe333f95b6f9355
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-18 12:25:58 +0500

    Create Const_Var_in_objects.js

---
+++ Const_Var_in_objects.js
@@ -0,0 +1,20 @@
+ <script>
+      /*
+
+                 Constant varibale  objects
+
+
+*/
+
+      const b = {
+        fullname: "Muhammad Shakir",
+        Age: 21,
+      };
+
+      document.write(b.fullname + " " + b.Age + "<br><br>");
+
+      b.fullname = "Anum Mustafa";
+      b.Age = 22;
+
+      document.write(b.fullname + " " + b.Age + "<br>");
+    </script>
